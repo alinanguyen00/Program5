747a35fdb1f087445775fdeba0cac6f08df29c42
alinanguyen00 <45619500+alinanguyen00@users.noreply.github.com>
2018-12-05 12:49:41 -0600

    Update Main.asm

--- Main.asm
+++ Main.asm
@@ -19,9 +19,24 @@
 
 
 ; start of actual program
+loop
+LDI R0, Buffer
+BRz loop
+TRAP x21
+AND R1, R1, #0
+STI R1, Buffer
+
+;checking the letter in R0 for start codon
+
+
+
+
+TRAP x25
+
 ADDRESS		.FILL x4000
 Int		.FILL x0180
 ISR1		.FILL x2600
 KBSR		.FILL xFE00
 KBDR		.FILL xFE02
+Buffer		.FILL x4600
 		.END
